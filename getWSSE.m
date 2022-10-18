f1cfcc5b63682109d8036c88b5ae8488c2bdf7f4
MaxineSherman <78165042+MaxineSherman@users.noreply.github.com>
2022-10-18 14:41:16 +0100

    [Upload]
    Uploaded getWSSE - a script to calculate within-subjects standard errors

---
+++ getWSSE.m
@@ -0,0 +1,41 @@
+% function WSSE = getWSSE(data)
+% 
+% This computes the within-subject standard error. Use this for repeated-measures designs
+% 
+% INPUTS:
+% data                   An nsubjects x nconditions matrix. All conditions should be repeated-measures
+%
+% OUTPUTS:
+% WSSE                   A 1 x nconditions vector containing the within-subject standard error for each condition
+%
+%
+% Maxine Sherman
+% m.sherman@sussex.ac.uk
+                
+
+function WSSE = getWSSE(data)
+
+
+[nsubj,ncond] = size(data);
+
+% Get grand mean
+grand_mean = nanmean(nanmean(data)); 
+
+% Get participant means
+p_mean = nanmean(data,2);
+
+% Get adjustment factors
+adj_factor =  - p_mean + grand_mean;
+
+% Adjust data
+data_adj = [];
+for p = 1:nsubj
+    data_adj(p,:) = data(p,:) + adj_factor(p);
+end
+
+% get SE
+WSSE=[];
+for c = 1:ncond
+    WSSE(1,c)    = nanstd(data_adj(:,c))/sqrt(nsubj);
+end
+
